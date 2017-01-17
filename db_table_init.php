0af879a5b53fdc65c06ac336bf706c81611464a5
Jenny Chen <jennychen@chenwanqide-Mac-mini.local>
2017-01-17 17:30:10 +0800

    Adjust : CSS styles

--- db_table_init.php
+++ db_table_init.php
@@ -5,7 +5,7 @@ $dbSetting = new DBSetting();
 try {
     $dbh = new PDO("mysql:host=$dbSetting->host;dbname=$dbSetting->dbname;port=$dbSetting->port", $dbSetting->user1, $dbSetting->user1pass);
 
-    $dbh->exec('create table account(name varchar(30), password varchar(8) CHARACTER SET utf8, primary key (name))')
+    $dbh->exec('create table account(name varchar(30) CHARACTER SET utf8, password varchar(8) CHARACTER SET utf8, primary key (name))')
         or error_log('table create failed', '3', 'guestbook.log');
     $dbh->exec('create table posts(id int NOT NULL AUTO_INCREMENT, author varchar(30), update_time DATETIME, title varchar(100) CHARACTER SET utf8, content varchar(2000) CHARACTER SET utf8, filepath varchar(100) CHARACTER SET utf8, primary key (id))')
             or error_log('table2 create failed', '3', 'guestbook.log');
